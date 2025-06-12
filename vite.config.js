721e7d6fb6abd0f64a8dba24a46150f7834e1353
thislily <lilyclairewatson@gmail.com>
2025-06-12 22:57:34 +0200

    pls

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server: {
-    port: 3000,
-    open: true
-  }
 })
